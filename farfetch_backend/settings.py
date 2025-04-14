dd7b314079e8588e2e28f9b424eb855f8c23f6ef
Abdul Samad <theasamad6@gmail.com>
2025-04-15 00:40:48 +0500

    initial commit

--- farfetch_backend/settings.py
+++ farfetch_backend/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v+y(2fqf#ffhm&h9lq)q_zl@ydva#$3x$e)dan6lkd=lq46b4f'
-
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
